4dc989b60cea38bb640458e7f3f4c24d23cf2e3a
KOU <142812925+fk0u@users.noreply.github.com>
2023-09-15 18:20:08 +0800

    Add files via upload

---
+++ eksponen.py
@@ -0,0 +1,9 @@
+# Meminta pengguna untuk memasukkan basis dan eksponen
+basis = float(input("Masukkan basis: "))
+eksponen = float(input("Masukkan eksponen: "))
+
+# Menghitung eksponen
+hasil = basis ** eksponen
+
+# Menampilkan hasil
+print(f"Hasil dari {basis} pangkat {eksponen} adalah {hasil}")
